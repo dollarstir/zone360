04755079260db203ecf81ebcab3df0bb7c9b09d8
Frederick Ennin <kpin463@gmail.com>
2022-11-14 02:01:45 +0000

    ui update 23

--- pay1.php
+++ pay1.php
@@ -31,24 +31,9 @@
                     <label class="mb-2" for="first-name">Enter Transaction ID after payment</label>
                     <input class="form-control mb-30" id="first-name" type="text" name="tid" placeholder="Mobile Money Transaction ID" value="" required>
                   </div>
-                  <!-- <div class="col-12">
-                    <label class="mb-2" for="last-name">Last Name</label>
-                    <input class="form-control mb-30" id="last-name" type="text" placeholder="Last Name" value="" required>
-                  </div>
+                  
                   <div class="col-12">
-                    <label class="mb-2" for="company">Company Name</label>
-                    <input class="form-control mb-30" id="company" type="text" placeholder="Company" value="">
-                  </div>
-                  <div class="col-12">
-                    <label class="mb-2" for="email-address">Email Address</label>
-                    <input class="form-control mb-30" id="email-address" type="email" placeholder="Email Address" value="" required>
-                  </div>
-                  <div class="col-12">
-                    <label class="mb-2" for="phone">Phone</label>
-                    <input class="form-control mb-30" id="phone" type="text" placeholder="Phone" value="">
-                  </div> -->
-                  <div class="col-12">
-                    <label class="mb-2" for="country">Select your Network Operator</label>
+                    <label class="mb-2" for="country">Select your network operator</label>
                     <select class="form-select form-control w-100 mb-30" id="country" name="operator">
                       <option value="mtn">MTN MOBILE MONEY</option>
                       <option value="vodafone">VODAFONE CASH</option>
@@ -58,8 +43,7 @@
                   </div>
                  
                   <div class="col-12">
-                    <label class="mb-2" for="order-notes">Order Notes (optional)</label>
-                    <textarea class="form-control" id="order-notes" placeholder="Notes about your order, e.g. special notes for delivery." rows="3"></textarea>
+                  <button class="btn btn-primary w-100 mt-4" type="submit">I have Paid</button>
                   </div>
                 </div>
               </form>
@@ -67,68 +51,7 @@
           </div>
           
           <!-- Order Form Area-->
-          <div class="col-12 col-lg-6">
-            <div class="order-form bg-gray">
-              <h4 class="mb-4">Order Sammury</h4>
-              <!-- Order Table-->
-              <div class="order-table table-responsive mb-4">
-                <table class="table table-bordered">
-                  <thead>
-                    <tr>
-                      <th class="px-3">Product </th>
-                      <th class="text-end px-3">Total</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <th class="px-3">Bonsai Tree <span> × 1</span></th>
-                      <td class="text-end px-3">$9.63</td>
-                    </tr>
-                    <tr>
-                      <th class="px-3">Sony BS4 <span> × 1</span></th>
-                      <td class="text-end px-3">$7.31</td>
-                    </tr>
-                  </tbody>
-                  <tfoot>
-                    <tr>
-                      <th class="px-3">Subtotal </th>
-                      <td class="text-end px-3">$16.94</td>
-                    </tr>
-                    <tr>
-                      <th class="px-3">Shipping </th>
-                      <td class="text-end px-3">Flat Rate $5.00</td>
-                    </tr>
-                    <tr>
-                      <th class="px-3">Order Total </th>
-                      <td class="text-end px-3">$21.94</td>
-                    </tr>
-                  </tfoot>
-                </table>
-              </div>
-              <!-- Checkout Payment-->
-              <div class="checkout-payment-area">
-                <form action="#">
-                  
-                  <div class="form-group">
-                    <div class="form-check">
-                      <input class="form-check-input" checked id="momo" type="radio" value="momo" name="checkout-payment">
-                      <img src="img/svg-icon/momo.png" style="height:60px;width:50%;"/>
-                      <!-- <label class="form-check-label font-weight-bold" for="momo">Mobile Money</label> -->
-                    </div>
-                    <div class="payment-meta">
-                      <p>Mobile Money</p>
-                    </div>
-                  </div>
-                  
-                  <div class="form-group mt-4">
-                    <!-- <p>Your personal data will be used to process your order, support your experience throughout this website, and for other purposes described in our privacy policy.</p> -->
-                  </div>
-                  <!-- Place Order-->
-                  <button class="btn btn-primary w-100 mt-4" type="submit">Proceed to payment</button>
-                </form>
-              </div>
-            </div>
-          </div>
+         
 
 
 
