b9dfd10abe4afc95f8671685b6d6b5c627cd6699
Frederick Ennin <kpin463@gmail.com>
2022-11-14 02:28:49 +0000

    ui update 25

--- tail.php
+++ tail.php
@@ -76,33 +76,7 @@
     </footer>
     <!-- floating button -->
 
-    <div class="fab-container">
-  <div class="fab shadow">
-    <div class="fab-content">
-      <span class="material-icons">support_agent</span>
-    </div>
-  </div>
-  <div class="sub-button shadow">
-    <a href="google.com" target="_blank">
-      <span class="material-icons">phone</span>
-    </a>
-  </div>
-  <div class="sub-button shadow">
-    <a href="google.com" target="_blank">
-      <span class="material-icons">mail_outline</span>
-    </a>
-  </div>
-  <div class="sub-button shadow">
-    <a href="google.com" target="_blank">
-      <span class="material-icons">language</span>
-    </a>
-  </div>
-  <div class="sub-button shadow">
-    <a href="google.com" target="_blank">
-      <span class="material-icons">help_outline</span>
-    </a>
-  </div>
-</div>
+    
     <!-- Scroll To Top -->
     <div id="scrollTopButton"><i class="bi bi-arrow-up-short"></i></div>
     <!-- Cookie Alert Area-->
