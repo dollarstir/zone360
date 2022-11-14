6b14aad3dc51f7676e61e7fa90987ec69762240d
Frederick Ennin <kpin463@gmail.com>
2022-11-14 11:00:58 +0000

    39

--- contact.php
+++ contact.php
@@ -51,11 +51,11 @@
                 <div class="row">
                   <div class="col-12 col-lg-6">
                     <label class="form-label" for="name">Full Name:</label>
-                    <input class="form-control mb-30" id="name" type="text" placeholder="Designing World" value="" name="name" required>
+                    <input class="form-control mb-30" id="name" type="text" placeholder="Name" value="" name="name" required>
                   </div>
                   <div class="col-12 col-lg-6">
                     <label class="form-label" for="email">Email Address:</label>
-                    <input class="form-control mb-30" id="email" type="email" placeholder="care.designingworld@gmail.com" name="email" value="" required>
+                    <input class="form-control mb-30" id="email" type="email" placeholder="Email Address" name="email" value="" required>
                   </div>
                   <div class="col-12">
                     <label class="form-label" for="subject">Topics:</label>
@@ -77,92 +77,9 @@
     </div>
     <div class="mb-120 d-block"></div>
     <!-- Google Maps-->
-    <div class="google-maps-wrapper">
+    <!-- <div class="google-maps-wrapper">
       <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14047.882048906753!2d-0.14268817855593444!3d51.50701170390822!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47d8a00baf21de75%3A0x52963a5addd52a99!2sLondon%2C%20UK!5e0!3m2!1sen!2sbd!4v1570696571917!5m2!1sen!2sbd" allowfullscreen=""></iframe>
-    </div>
+    </div> -->
     <div class="mb-120 d-block"></div>
     <!-- Footer Area-->
-    <footer class="footer-area">
-      <div class="container">
-        <div class="row g-4 g-lg-5">
-          <!-- Footer Widget Area -->
-          <div class="col-12 col-sm-6 col-lg-3">
-            <div class="footer-widget-area"><a class="d-block mb-4" href="index.html"><img src="img/core-img/logo.png" alt=""></a>
-              <p>It's crafted with the latest trend of design &amp; coded with all modern approaches.</p>
-              <!-- Newsletter Form -->
-              <div class="newsletter-form mb-4">
-                <form class="d-flex align-items-stretch" action="#">
-                  <input class="form-control rounded-0 rounded-start" type="email" placeholder="Enter email">
-                  <button class="btn btn-warning rounded-0 rounded-end px-3" type="submit"><i class="bi bi-arrow-right"></i></button>
-                </form>
-              </div>
-            </div>
-          </div>
-          <!-- Footer Widget Area-->
-          <div class="col-12 col-sm-6 col-lg-3">
-            <div class="footer-widget-area">
-              <h5 class="mb-4">Our Products</h5>
-              <ul class="list-unstyled">
-                <li><a href="#" target="_blank"><i class="bi bi-caret-right"></i>Suha Mobile</a></li>
-                <li><a href="#" target="_blank"><i class="bi bi-caret-right"></i>Affan Mobile</a></li>
-                <li><a href="#" target="_blank"><i class="bi bi-caret-right"></i>Newsten Blog</a></li>
-                <li><a href="#" target="_blank"><i class="bi bi-caret-right"></i>Classy Multipurpose</a></li>
-                <li><a href="#" target="_blank"><i class="bi bi-caret-right"></i>Educamp Education</a></li>
-              </ul>
-            </div>
-          </div>
-          <!-- Footer Widget Area-->
-          <div class="col-12 col-sm-6 col-lg-3">
-            <div class="footer-widget-area">
-              <h5 class="mb-4">Important Links</h5>
-              <ul class="list-unstyled">
-                <li><a href="#" target="_blank"><i class="bi bi-caret-right"></i>Community &amp; Forum</a></li>
-                <li><a href="#" target="_blank"><i class="bi bi-caret-right"></i>About Licences</a></li>
-                <li><a href="#" target="_blank"><i class="bi bi-caret-right"></i>Careers</a></li>
-                <li><a href="#" target="_blank"><i class="bi bi-caret-right"></i>Terms &amp; Conditions</a></li>
-                <li><a href="#" target="_blank"><i class="bi bi-caret-right"></i>Privacy Policy</a></li>
-              </ul>
-            </div>
-          </div>
-          <!-- Footer Widget Area -->
-          <div class="col-12 col-sm-6 col-lg-3">
-            <div class="footer-widget-area">
-              <h5 class="mb-4">Our Location</h5>
-              <p class="lh-base mb-3">Designing World Inc. <br> 1604 NSD, Dhaka, BD</p>
-              <p class="mb-0">Call: +123 456 789 <br> Email: help@example.com</p>
-              <!-- Footer Social Icon -->
-              <div class="footer-social-icon d-flex align-items-center mt-3"><a href="#" data-bs-toggle="tooltip" data-bs-placement="top" title="Facbook"><i class="bi bi-facebook"></i></a><a href="#" data-bs-toggle="tooltip" data-bs-placement="top" title="Twitter"><i class="bi bi-twitter"></i></a><a href="#" data-bs-toggle="tooltip" data-bs-placement="top" title="Instagram"><i class="bi bi-instagram"></i></a><a href="#" data-bs-toggle="tooltip" data-bs-placement="top" title="Linkedin"><i class="bi bi-linkedin"></i></a><a href="#" data-bs-toggle="tooltip" data-bs-placement="top" title="Youtube"><i class="bi bi-youtube"></i></a></div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="container">
-        <div class="copywrite-wrapper mt-5 rounded d-lg-flex align-items-lg-center justify-content-lg-between">
-          <!-- Copywrite Text -->
-          <div class="copywrite-text text-center text-lg-start mb-3 mb-lg-0 me-lg-4">
-            <p class="mb-0">2021 &copy; All rights reserved by <a href="#" target="_blank">Designing World</a></p>
-          </div>
-          <!-- Footer Nav -->
-          <div class="footer-nav mb-3 mb-lg-0 me-lg-4">
-            <ul class="mb-0 d-flex flex-wrap justify-content-center list-unstyled">
-              <li><a href="#" target="_blank">Privacy Policy</a></li>
-              <li><a href="#" target="_blank">Terms &amp; Conditions</a></li>
-              <li><a href="#" target="_blank">Get Support</a></li>
-            </ul>
-          </div>
-          <!-- Dropup -->
-          <div class="language-dropdown text-center text-lg-end">
-            <button class="btn dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">Language</button>
-            <div class="dropdown-menu dropdown-menu-end"><a class="dropdown-item" href="#"><span class="me-2 flag-icon flag-icon-sa"></span>Arabic</a><a class="dropdown-item" href="#"><span class="me-2 flag-icon flag-icon-bd"></span>Bengali</a><a class="dropdown-item" href="#"><span class="me-2 flag-icon flag-icon-us"></span>English</a><a class="dropdown-item" href="#"><span class="me-2 flag-icon flag-icon-my"></span>Malay</a><a class="dropdown-item" href="#"><span class="me-2 flag-icon flag-icon-es"></span>Spanish</a></div>
-          </div>
-        </div>
-      </div>
-    </footer>
-    <!-- Scroll To Top  -->
-    <div id="scrollTopButton"><i class="bi bi-arrow-up-short"></i></div>
-    <div class="mb-120 d-block"></div>
-    <!-- All JavaScript Files-->
-    <script src="js/all-js-libraries.js"></script>
-    <script src="js/active.js"></script>
-  </body>
-</html>
+    <?php include 'tail.php'; ?>
